36a04cce834ed7586f74ac57dee33fb2717101ad
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2017-11-09 18:16:01 -0800

    First: sprites and bounce

---
+++ Assets/Scripts/TrampoBounce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampoBounce : MonoBehaviour {
+
+	public GameObject player;
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.tag == "Player") {
+			Debug.Log ("True");
+			player.transform.position = new Vector2 (transform.position.x, 0f);
+		}
+	}
+}
