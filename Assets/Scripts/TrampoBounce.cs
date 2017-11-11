5c81afade13b6cbb9894a2ed4a8f057bd03993e9
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2017-11-10 17:26:40 -0800

    Redid sprites

--- Assets/Scripts/TrampoBounce.cs
+++ Assets/Scripts/TrampoBounce.cs
@@ -1,15 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrampoBounce : MonoBehaviour {
 
 	public GameObject player;
+	public Text timerText;
+	public Text levelText;
+
+	private int level = 1;
+	private float timeElapsed = 0.0f;
+	private float timer = 0.0f;
+	private float timerStart = 0.0f;
+
+	void Start () {
+		timerText.text = "Time Elapsed: " + timeElapsed;
+		levelText.text = "Level: " + level;
+	}
+
+	void Update () {
+		timer += Time.deltaTime;
+	}
 
 	void OnCollisionEnter2D(Collision2D collision){
 		if (collision.gameObject.tag == "Player") {
 			Debug.Log ("True");
-			player.transform.position = new Vector2 (transform.position.x, 0f);
+			timeElapsed = timer - timerStart;
+			timerText.text = "Time Elapsed: " + timeElapsed;
+			timerStart = timer;
+			level += 1;
+			levelText.text = "Level: " + level;
 		}
 	}
 }
---
+++ Assets/Scripts/TrampoScroll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TrampoScroll: MonoBehaviour
+{
+	public ScrollRect myScrollRect;
+
+	private float scroll = .01f;
+
+	void Start() {
+		myScrollRect.horizontalNormalizedPosition = -2.6f;
+	}
+
+	void Update() {
+		myScrollRect.horizontalNormalizedPosition = myScrollRect.horizontalNormalizedPosition + scroll;
+	}
+}
