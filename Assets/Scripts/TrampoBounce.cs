6f3e3dac793c687de5e4b1f7b31b7f5333f52c4b
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2017-11-11 23:08:59 -0800

    Game is playable

--- Assets/Scripts/TrampoBounce.cs
+++ Assets/Scripts/TrampoBounce.cs
@@ -5,32 +5,24 @@ using UnityEngine.UI;
 
 public class TrampoBounce : MonoBehaviour {
 
-	public GameObject player;
-	public Text timerText;
 	public Text levelText;
-
-	private int level = 1;
-	private float timeElapsed = 0.0f;
-	private float timer = 0.0f;
-	private float timerStart = 0.0f;
+	public Text winText;
+	public Text loseText;
+	public Text restartText;
+	public int level = 0;
+	public bool entered = false;
 
 	void Start () {
-		timerText.text = "Time Elapsed: " + timeElapsed;
 		levelText.text = "Level: " + level;
-	}
-
-	void Update () {
-		timer += Time.deltaTime;
+		winText.text = "";
+		loseText.text = "";
+		restartText.text = "";
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
 		if (collision.gameObject.tag == "Player") {
-			Debug.Log ("True");
-			timeElapsed = timer - timerStart;
-			timerText.text = "Time Elapsed: " + timeElapsed;
-			timerStart = timer;
-			level += 1;
-			levelText.text = "Level: " + level;
+				entered = true;
 		}
 	}
 }
+
---
+++ Assets/Scripts/exitApp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitApp : MonoBehaviour {
+
+	public void ExitApp() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		Debug.Log ("Unity Editor");
+		#else
+		Application.Quit();
+		Debug.Log ("Quit");
+		#endif
+	}
+}
