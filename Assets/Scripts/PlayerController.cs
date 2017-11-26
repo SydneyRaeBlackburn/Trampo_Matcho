66d90943973c885420aaf5504a95e8de3c350fee
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2017-11-25 20:06:25 -0800

    Created a script the rotates the players arms and legs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//private float rotationAmount = 10.0f;
+
+	void Update() {
+		transform.rotation = Quaternion.Euler (0, 0, (Mathf.PingPong (Time.time, 20f) - 10));
+	}
+}
