bf3b83b42777d88b817b874350e8b0499c2574e1
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2017-11-26 20:36:49 -0800

    Created buttons on the game to go to the Menu or Credits scene and did some clean up

--- Assets/Scripts/TrampoBounce.cs
+++ Assets/Scripts/TrampoBounce.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class TrampoBounce : MonoBehaviour {
-
-	//public Text levelText;
-	//public Text winText;
-	//public Text loseText;
-	//public Text restartText;
-	//public int level = 0;
+	
 	public bool entered = false;
 	public AudioClip jumpEffect;
-
-	void Start () {
-		//levelText.text = "Level: " + level;
-		//winText.text = "";
-		//loseText.text = "";
-		//restartText.text = "";
-	}
 
 	void OnCollisionEnter2D(Collision2D collision){
 		if (collision.gameObject.tag == "Player") {
